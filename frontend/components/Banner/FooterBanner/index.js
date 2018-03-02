5611d305a85383a000b0fa41954ea25f46c3e661
sydbal <dominic.balassone@gmail.com>
2018-03-02 13:31:41 -0800

    simplified emoji equation

--- frontend/components/Banner/FooterBanner/index.js
+++ frontend/components/Banner/FooterBanner/index.js
@@ -11,10 +11,10 @@ class FooterBanner extends React.Component {
                         This site is under construction.
                     </p>
                     <kbd>
-                        const code = (❤️, &#x1F35D;, ☕) => ({'{quality: '}❤️, speed: Math.pow(&#x1F35D;, ☕){'}'})
+                        const code = (❤️, ☕) => ({'{quality: '}❤️, speed: Math.pow(❤️, ☕){'}'})
                     </kbd>
                     <p>
-                        <br/>Send &#x1F35D; ☕ and ❤️ to<br/>
+                        <br/>Send ☕ and ❤️ to<br/>
                         BTC: 1FirT3AVP3qcNxaemdpdPSAxSqcEUsn3Un<br/>
                         ETH: 0x39e6745e5b4ac07189f92647206f63c152b66c30
                     </p>
