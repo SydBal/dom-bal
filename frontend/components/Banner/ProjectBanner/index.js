0b96cf4a76f5bda3f0f42f2b6169e2dc7c0c8ff7
sydbal <dominic.balassone@gmail.com>
2018-03-03 15:11:41 -0800

    preparing page for projects using row/cols

--- frontend/components/Banner/ProjectBanner/index.js
+++ frontend/components/Banner/ProjectBanner/index.js
@@ -7,8 +7,17 @@ class ProjectBanner extends React.Component {
         return (
             <div id="projectBanner" className="banner container-fluid bg-clear text-white">
                 <div className="container text-center">
-                    <h1 className="display-1">Projects</h1>
-                    <h1>Coming Soon!</h1>
+                    <h1>Projects</h1>
+                    <div className="row justify-content-center">
+                        <div className="col-sm-4">Redoot</div>
+                        <div className="col-sm-4">FitBit</div>
+                        <div className="col-sm-4">Heatmap</div>
+                        <div className="col-sm-4">Tuvalu</div>
+                        <div className="col-sm-4">256 Bitmap</div>
+                        <div className="col-sm-4">Philae Lander</div>
+                        <div className="col-sm-4">ML Silhouette</div>
+
+                    </div>
                 </div>
             </div>
 		);
