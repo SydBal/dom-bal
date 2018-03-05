a4229018827682957c61726c5276bce4c432b3ca
sydbal <dominic.balassone@gmail.com>
2018-03-05 11:23:20 -0800

    working on project segments, added cards

--- frontend/components/Banner/ProjectBanner/index.js
+++ frontend/components/Banner/ProjectBanner/index.js
@@ -9,14 +9,41 @@ class ProjectBanner extends React.Component {
                 <div className="container text-center">
                     <h1>Projects</h1>
                     <div className="row justify-content-center">
-                        <div className="col-sm-4">Redoot</div>
-                        <div className="col-sm-4">FitBit</div>
-                        <div className="col-sm-4">Heatmap</div>
-                        <div className="col-sm-4">Tuvalu</div>
-                        <div className="col-sm-4">256 Bitmap</div>
-                        <div className="col-sm-4">Philae Lander</div>
-                        <div className="col-sm-4">ML Silhouette</div>
-
+                        <div className="col-md-4 col-sm-6">
+                            <div className="projectCard">
+                                Redoot
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-sm-6">
+                            <div className="projectCard">
+                                FitBit
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-sm-6">
+                            <div className="projectCard">
+                                Heatmap
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-sm-6">
+                            <div className="projectCard">
+                                Tuvalu
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-sm-6">
+                            <div className="projectCard">
+                                256 Bitmap
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-sm-6">
+                            <div className="projectCard">
+                                Philae Lander
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-sm-6">
+                            <div className="projectCard">
+                                ML Silhouette
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
